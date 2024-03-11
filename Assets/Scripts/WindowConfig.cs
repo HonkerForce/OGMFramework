35fc5fe9600038a914fa5c92da55aceb23fe916f
HonkerForce <scorpiowolf@qq.com>
2024-03-11 14:47:28 +0800

    引入Addressable插件，修改WinModel为WindowConfig，增加创建ScriptableObject资源的逻辑

---
+++ Assets/Scripts/WindowConfig.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace OGMFramework
+{
+	public enum WindowModel
+	{
+		None = -1,
+		Test,
+		Test1,
+		Max
+	}
+
+	public static class CreateWindowConfigTool
+	{
+		private static WindowConfig config;
+		
+		[MenuItem("Tools/Create UI Configs File")]
+		public static void CreateConfig()
+		{
+			config = ScriptableObject.CreateInstance<WindowConfig>();
+			if (AssetDatabase.IsValidFolder("Assets/Configs") == false)
+			{
+				AssetDatabase.CreateFolder("Assets", "Configs");
+			}
+			AssetDatabase.CreateAsset(config, "Assets/Configs/NewUIWinConfigs.asset");
+			AssetDatabase.SaveAssets();
+			AssetDatabase.Refresh();
+		}
+	}
+
+	[Serializable]
+	[CreateAssetMenu(fileName = "NewUIWinConfigs", menuName = "UIWindowConfigs", order = 1)]
+	public class WindowConfig : ScriptableObject
+	{
+		public WindowModel winModel;
+		public AssetReferenceGameObject prefabRef;
+		public bool isHideWhenSceneChanged;
+		public List<SubWindowConfig> subWinConfigs;
+	}
+
+	[Serializable]
+	public class SubWindowConfig
+	{
+		public WindowModel winModel;
+		public AssetReferenceGameObject prefabRef;
+		public string parentRoot;
+	}
+}
